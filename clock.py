b8d5160d2ec515ee90366b46e8a6cec88c69e3be
PerseusKyogre09 <kyogre.perseus09@gmail.com>
2024-11-08 18:36:33 +0530

    Minimize to tray feature

--- clock.py
+++ clock.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import QTimer, Qt, QDateTime, QPoint, QSize
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton
-from PyQt5.QtGui import QFontDatabase, QFont, QCursor
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QPushButton, QSystemTrayIcon, QMenu
+from PyQt5.QtGui import QFontDatabase, QFont, QCursor, QIcon
 import sys
 import os
 
@@ -45,8 +45,46 @@ class TransparentClockWidget(QWidget):
         self.close_button.setFont(QFont("Arial", 12, QFont.Bold))
         self.close_button.setStyleSheet("background-color: transparent; color: white; border: none;")
         self.close_button.hide()
-        self.close_button.clicked.connect(self.close)
+        self.close_button.clicked.connect(self.hide_to_tray)
+        self.close_button.move(0, 20)
         
+        # System tray
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon("images/clock_icon.png"))
+        self.tray_icon.setToolTip("Transparent Clock")
+        
+        # Tray menu
+        tray_menu = QMenu()
+        tray_menu.setStyleSheet("""
+            QMenu {
+                background-color: #333;
+                border: 1px solid #555;
+                padding: 10px;
+                border-radius: 10px;
+            }
+            QMenu::item {
+                color: white;
+                padding: 8px 20px;
+                background-color: transparent;
+            }
+            QMenu::item:selected {
+                background-color: #444;
+            }
+            QMenu::separator {
+                height: 1px;
+                background: #666;
+                margin: 5px 0;
+            }
+        """)
+
+        show_action = tray_menu.addAction("Show")
+        show_action.triggered.connect(self.show)
+        exit_action = tray_menu.addAction("Exit")
+        exit_action.triggered.connect(QApplication.quit)
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.activated.connect(self.on_tray_icon_click)
+        self.tray_icon.show()
+
         # Mouse tracker
         self.setMouseTracking(True)
         self.closeButtonHover = False
@@ -70,6 +108,23 @@ class TransparentClockWidget(QWidget):
         self.time_date_label.setText(display_text)
         self.adjustSize()
 
+    def hide_to_tray(self):
+        self.hide()
+        # Shows message when closed
+        self.tray_icon.showMessage(
+            "Transparent Clock",
+            "The Clock widget has been minimized to the system tray.",
+            QSystemTrayIcon.Information,
+            3000
+        )
+
+    # Opens widget if icon is clicked in tray
+    def on_tray_icon_click(self, reason):
+        if reason == QSystemTrayIcon.Trigger:
+            if not self.isVisible():
+                self.show()
+                self.activateWindow()
+
     def mousePressEvent(self, event):
         self.old_pos = event.globalPos()
 
@@ -86,6 +141,7 @@ class TransparentClockWidget(QWidget):
         self.close_button.hide()
 
     def closeEvent(self, event):
+        self.tray_icon.hide()
         event.accept()
 
 if __name__ == "__main__":
